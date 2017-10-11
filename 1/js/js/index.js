68e07c95a05206ae28db2a70f8a9393b0bf42c9d
gc18773375617 <654992676@qq.com>
2017-10-11 09:43:59 +0800

    文件整理

---
+++ 1/js/js/index.js
@@ -0,0 +1,10 @@
+document.write("姓名:刘德华</br>");
+document.write("性别:男"+"</br>");
+document.write("别名:华仔"+"</br>");
+document.write("爱好:唱歌"+"</br>");
+document.write("身高:174cm"+"</br>");
+document.write("体重:63kg"+"</br>");
+var num=parseInt(prompt("请输入数字"));
+
+num=num+1000;
+alert(num);
